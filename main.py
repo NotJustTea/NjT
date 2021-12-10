f783cb288822e07941d7a63c12a7656b574027ed
NotJustTea <95843996+NotJustTea@users.noreply.github.com>
2021-12-10 06:37:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import telebot
 bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(content_types=['text'])
-@bot.message_handler(content_types=)
 def message_recieved(message):
     print(message)
     bot.send_message(message.chat.id, message.text)
